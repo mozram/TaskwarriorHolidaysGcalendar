e56ce6ff096aef5e2c537fdbc75cd870bfefd1e8
Mohd Zulhilmi Ramli <zulhilmi.ramli@outlook.my>
2022-11-21 21:11:55 +0800

    use my holiday calendar

--- getTwHolidaysGcal.py
+++ getTwHolidaysGcal.py
@@ -1,4 +1,4 @@
-#!usr/bin/env python3
+#!/bin/python3
 #CREATED BY: Pedro Rene Lima Camacho
 #DESCRIPTION: parse a google ics holiday calendar to taskwarrior format
 #   for using it with `task calendar` command.
@@ -11,14 +11,14 @@ from ics import Calendar
 
 #TODO make a way to select other calendar from google calendars
 #we can change the part es.bo to en.us to download US holidays instead
-url = "https://calendar.google.com/calendar/ical/es.bo%23holiday%40group.v.calendar.google.com/public/basic.ics"
+url = "https://calendar.google.com/calendar/ical/en.malaysia%23holiday%40group.v.calendar.google.com/public/basic.ics"
 cal = Calendar(requests.get(url).text)
 
 inc=0
 #TODO make a way to write to a file
 #TODO change strings according the country or calendar downloaded
 for event in cal.timeline:
-  r1 = f'holiday.es-BO{inc}.name={event.name}'
-  r2 = f'holiday.es-BO{inc}.date={event.begin.format("YYYYMMDD")}'
+  r1 = f'holiday.en-MY{inc}.name={event.name}'
+  r2 = f'holiday.en-MY{inc}.date={event.begin.format("YYYYMMDD")}'
   inc+=1
   print(r1, r2, sep='\n')
